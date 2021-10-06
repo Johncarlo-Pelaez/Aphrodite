fec0dbeba4373e8aa9c905a93123e34271666725
Renz Isla <renz.mbisla@gmail.com>
2021-10-06 08:57:17 +0000

    Swagger dev mode only

--- main-api/src/main.ts
+++ main-api/src/main.ts
@@ -1,7 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppConfigModule, AppConfigService } from './app-config';
+import { AppConfigModule, AppConfigService, Environment } from './app-config';
 import { AppModule } from './app.module';
 
 const logger = new Logger('Main');
@@ -14,12 +14,14 @@ async function bootstrap() {
     .select(AppConfigModule)
     .get(AppConfigService, { strict: true });
 
-  const config = new DocumentBuilder()
-    .setTitle("Amicassa's RIS Main API")
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api', app, document);
+  if (appConfigService.nodeEnv === Environment.Development) {
+    const config = new DocumentBuilder()
+      .setTitle("Amicassa's RIS Main API")
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api', app, document);
+  }
 
   await app.listen(appConfigService.port);
   const url = await app.getUrl();
