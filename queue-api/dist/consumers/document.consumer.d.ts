90d42756f84e1722b07fbdf730c50719ca611831
Eduard G. Peran <egperan@epdsi.ph>
2021-10-12 18:59:03 +0800

    removed dist folder from deleted queue-api

--- queue-api/dist/consumers/document.consumer.d.ts
+++
@@ -1,7 +0,0 @@
-import { Job } from 'bull';
-import { DocumentRepository } from 'src/repositories';
-export declare class DocumentConsumer {
-    private readonly documentRepository;
-    constructor(documentRepository: DocumentRepository);
-    migrate(job: Job<number>): Promise<void>;
-}
--- queue-api/dist/repositories/document.repository.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'src/entities';
-import { EntityManager } from 'typeorm';
-export declare class DocumentRepository {
-    private readonly manager;
-    constructor(manager: EntityManager);
-    getDocument(id: number): Promise<Document>;
-}
