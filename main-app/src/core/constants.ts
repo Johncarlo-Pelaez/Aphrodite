cb8aef83f3da7b89dddbd821608de934ed7ea89f
BjornFerrer <lollimelt.bf@gmail.com>
2021-10-12 23:15:50 +0800

    aws config, upload starting item

---
+++ main-app/src/core/constants.ts
@@ -0,0 +1,9 @@
+export { STARTING_ITEM, accessKeyId, secretAccessKey, S3_BUCKET, REGION };
+
+const STARTING_ITEM = 1;
+
+const accessKeyId = 'AKIA53VLFJXVVYTNO23G';
+const secretAccessKey = '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';
+
+const S3_BUCKET = 'epds-aphrodite';
+const REGION = 'ap-southeast-1';
