ce8c177ecd742962a8e8c7185b85af74f5111952
BjornFerrer <lollimelt.bf@gmail.com>
2021-10-12 23:19:05 +0800

    homepage button new upload, delete old s3 upload single file

--- main-app/src/pages/home-page/HomePage.tsx
+++ main-app/src/pages/home-page/HomePage.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
-import { S3Upload } from './S3Upload';
+import { useState } from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { UploadFilesModal } from './components';
 
 export const HomePage = () => {
-  return <S3Upload />;
+  const [modalShow, setModalShow] = useState(false);
+  const handleShow = () => setModalShow(true);
+
+  return (
+    <Container>
+      <div className="homepage">
+        <Button className="mt-5" variant="primary" onClick={handleShow}>
+          New
+        </Button>
+        <UploadFilesModal
+          isVisible={modalShow}
+          onClose={() => setModalShow(false)}
+        />
+      </div>
+    </Container>
+  );
 };
---
+++ main-app/src/pages/home-page/components/index.ts
@@ -0,0 +1,2 @@
+export { S3Uploader } from './s3-uploader';
+export { UploadFilesModal } from './upload-files-modal';
