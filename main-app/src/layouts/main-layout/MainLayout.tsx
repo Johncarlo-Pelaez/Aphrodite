68c85670c5919fb5393de96ae79e24f0c996bd8a
Eduard G. Peran <egperan@epdsi.ph>
2021-10-21 17:08:42 +0800

    Changed navbar style

--- main-app/src/layouts/main-layout/MainLayout.tsx
+++ main-app/src/layouts/main-layout/MainLayout.tsx
@@ -4,6 +4,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { WithChildren } from 'core/types';
 import { useSignOut, useGetCurrentSignInUserName } from 'hooks/auth';
 import { getMainMenuItems } from './main-layout.config';
@@ -26,31 +28,30 @@ export const MainLayout = (props: MainLayoutProps): ReactElement => {
 
   return (
     <Fragment>
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar expand="lg">
         <Container>
-          <Navbar.Brand onClick={redirectToHome}>Amicassa</Navbar.Brand>
+          <Navbar.Brand onClick={redirectToHome}>
+            <strong>APP LOGO</strong>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav>
               {getMainMenuItems().map((mi, index) => (
                 <Nav.Link key={index} onClick={() => redirect(mi.path)}>
-                  {mi.label}
+                  <strong>{mi.label}</strong>
                 </Nav.Link>
               ))}
-            </Nav>
-            <Nav>
-              <Navbar.Text>Signed in as: </Navbar.Text>
-              <NavDropdown
-                id="nav-dropdown-dark-example"
-                title={username}
-                menuVariant="dark"
-              >
+              <span className="nav-link__devider"></span>
+              <NavDropdown title={username}>
                 <NavDropdown.Item>Account</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item onClick={signOutAsync}>
                   Log out
                 </NavDropdown.Item>
               </NavDropdown>
+              <span className="nav-link__avatar">
+                <FontAwesomeIcon icon={faUserCircle} size="2x" />
+              </span>
             </Nav>
           </Navbar.Collapse>
         </Container>
