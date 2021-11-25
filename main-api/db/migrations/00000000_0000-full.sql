ee574232f87a9538d6eaf7f54dbce45ccfc33da4
Eduard G. Peran <egperan@epdsi.ph>
2021-11-25 12:39:00 +0800

    Update full sql

--- main-api/db/migrations/00000000_0000-full.sql
+++ main-api/db/migrations/00000000_0000-full.sql
@@ -1,4 +1,4 @@
-/****** Object:  Table [dbo].[document]    Script Date: 11/23/2021 10:02:49 AM ******/
+/****** Object:  Table [dbo].[document]    Script Date: 11/25/2021 12:38:28 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -6,12 +6,12 @@ GO
 CREATE TABLE [dbo].[document](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[uuid] [uniqueidentifier] NOT NULL,
-	[userId] [int] NOT NULL,
+	[userUsername] [nvarchar](255) NOT NULL,
 	[documentName] [nvarchar](255) NOT NULL,
 	[documentSize] [int] NOT NULL,
 	[description] [nvarchar](500) NOT NULL,
 	[modifiedDate] [datetime] NOT NULL,
-	[modifiedBy] [int] NOT NULL,
+	[modifiedBy] [nvarchar](255) NULL,
 	[status] [nvarchar](100) NOT NULL,
 	[qrCode] [nvarchar](250) NULL,
 	[qrAt] [datetime] NULL,
@@ -24,11 +24,11 @@ CREATE TABLE [dbo].[document](
 	[contractDetailsReqParams] [nvarchar](max) NULL,
 	[springReqParams] [nvarchar](max) NULL,
 	[documentDate] [nvarchar](150) NULL,
-	[encoder] [int] NULL,
+	[encoder] [nvarchar](255) NULL,
 	[encodedAt] [datetime] NULL,
-	[checker] [int] NULL,
+	[checker] [nvarchar](255) NULL,
 	[checkedAt] [datetime] NULL,
-	[approver] [int] NULL,
+	[approver] [nvarchar](255) NULL,
 	[encodeValues] [nvarchar](max) NULL,
  CONSTRAINT [PK_document] PRIMARY KEY CLUSTERED 
 (
@@ -36,7 +36,7 @@ CREATE TABLE [dbo].[document](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[document_history]    Script Date: 11/23/2021 10:02:49 AM ******/
+/****** Object:  Table [dbo].[document_history]    Script Date: 11/25/2021 12:38:28 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -47,14 +47,14 @@ CREATE TABLE [dbo].[document_history](
 	[documentSize] [int] NOT NULL,
 	[createdDate] [datetime] NOT NULL,
 	[documentId] [int] NOT NULL,
-	[userId] [int] NOT NULL,
+	[userUsername] [nvarchar](255) NOT NULL,
  CONSTRAINT [PK_0783dd4cd636039459ee63c9a8b] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[lookup]    Script Date: 11/23/2021 10:02:49 AM ******/
+/****** Object:  Table [dbo].[lookup]    Script Date: 11/25/2021 12:38:28 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -65,7 +65,7 @@ CREATE TABLE [dbo].[lookup](
 	[documentGroup] [nvarchar](max) NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[nomenclature]    Script Date: 11/23/2021 10:02:49 AM ******/
+/****** Object:  Table [dbo].[nomenclature]    Script Date: 11/25/2021 12:38:28 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -75,14 +75,14 @@ CREATE TABLE [dbo].[nomenclature](
 	[description] [nvarchar](255) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[user]    Script Date: 11/23/2021 10:02:49 AM ******/
+/****** Object:  Table [dbo].[user]    Script Date: 11/25/2021 12:38:28 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[user](
 	[id] [int] IDENTITY(1,1) NOT NULL,
-	[email] [nvarchar](255) NOT NULL,
+	[username] [nvarchar](255) NOT NULL,
 	[firstName] [nvarchar](255) NULL,
 	[lastName] [nvarchar](255) NULL,
 	[role] [nvarchar](50) NOT NULL,
@@ -92,7 +92,7 @@ CREATE TABLE [dbo].[user](
 	[modifiedDate] [datetime] NULL,
  CONSTRAINT [PK_cace4a159ff9f2512dd42373760] PRIMARY KEY CLUSTERED 
 (
-	[id] ASC
+	[username] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -100,18 +100,18 @@ ALTER TABLE [dbo].[user] ADD  CONSTRAINT [DF_user_IsActive]  DEFAULT ((1)) FOR [
 GO
 ALTER TABLE [dbo].[user] ADD  CONSTRAINT [DF_user_isDeleted]  DEFAULT ((0)) FOR [isDeleted]
 GO
-ALTER TABLE [dbo].[document]  WITH CHECK ADD  CONSTRAINT [FK_7424ddcbdf1e9b067669eb0d3fd] FOREIGN KEY([userId])
-REFERENCES [dbo].[user] ([id])
+ALTER TABLE [dbo].[document]  WITH CHECK ADD  CONSTRAINT [FK_document_user] FOREIGN KEY([userUsername])
+REFERENCES [dbo].[user] ([username])
 GO
-ALTER TABLE [dbo].[document] CHECK CONSTRAINT [FK_7424ddcbdf1e9b067669eb0d3fd]
-GO
-ALTER TABLE [dbo].[document_history]  WITH CHECK ADD  CONSTRAINT [FK_3c5d2cf18513d9baa390fdf3260] FOREIGN KEY([userId])
-REFERENCES [dbo].[user] ([id])
-GO
-ALTER TABLE [dbo].[document_history] CHECK CONSTRAINT [FK_3c5d2cf18513d9baa390fdf3260]
+ALTER TABLE [dbo].[document] CHECK CONSTRAINT [FK_document_user]
 GO
 ALTER TABLE [dbo].[document_history]  WITH CHECK ADD  CONSTRAINT [FK_4a9486398505240b92812660f7f] FOREIGN KEY([documentId])
 REFERENCES [dbo].[document] ([id])
 GO
 ALTER TABLE [dbo].[document_history] CHECK CONSTRAINT [FK_4a9486398505240b92812660f7f]
+GO
+ALTER TABLE [dbo].[document_history]  WITH CHECK ADD  CONSTRAINT [FK_document_history_user] FOREIGN KEY([userUsername])
+REFERENCES [dbo].[user] ([username])
+GO
+ALTER TABLE [dbo].[document_history] CHECK CONSTRAINT [FK_document_history_user]
 GO
