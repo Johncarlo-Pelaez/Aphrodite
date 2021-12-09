9e03291ce6fd25d579cf1a23070f24cbedfbcecf
Eduard G. Peran <egperan@epdsi.ph>
2021-12-09 18:35:53 +0800

    just added tab

--- main-api/src/repositories/report/reports.queries.ts
+++ main-api/src/repositories/report/reports.queries.ts
@@ -1,36 +1,35 @@
 export const FIND_INFORMATION_REQUEST_REPORTS = `
-SELECT 
-document_history.createdDate AS requestedDate,
-document.documentName AS filename,
-document.encoder,
-document.qrCode,
-document.documentType,
-document.documentSize,
-document.pageTotal,
-document_history.documentStatus,
-document_history.salesforceResponse AS note
-FROM document_latest_info_request AS t
-INNER JOIN document_history
-ON t.documentId = document_history.documentId
-AND t.updatedDate = document_history.createdDate
-INNER JOIN document 
-ON document_history.documentId = document.id
-`;
+    SELECT 
+    document_history.createdDate AS requestedDate,
+    document.documentName AS filename,
+    document.encoder,
+    document.qrCode,
+    document.documentType,
+    document.documentSize,
+    document.pageTotal,
+    document_history.documentStatus,
+    document_history.salesforceResponse AS note
+    FROM document_latest_info_request AS t
+    INNER JOIN document_history
+    ON t.documentId = document_history.documentId
+    AND t.updatedDate = document_history.createdDate
+    INNER JOIN document 
+    ON document_history.documentId = document.id`;
 
 export const COUNT_INFORMATION_REQUEST_REPORTS = `
-SELECT 
-document_history.createdDate AS requestedDate,
-document.documentName AS filename,
-document.encoder,
-document.qrCode,
-document.documentType,
-document.documentSize,
-document.pageTotal,
-document_history.documentStatus,
-document_history.salesforceResponse AS note
-FROM document_latest_info_request AS t
-INNER JOIN document_history
-ON t.documentId = document_history.documentId
-AND t.updatedDate = document_history.createdDate
-INNER JOIN document 
-ON document_history.documentId = document.id`;
+    SELECT 
+    document_history.createdDate AS requestedDate,
+    document.documentName AS filename,
+    document.encoder,
+    document.qrCode,
+    document.documentType,
+    document.documentSize,
+    document.pageTotal,
+    document_history.documentStatus,
+    document_history.salesforceResponse AS note
+    FROM document_latest_info_request AS t
+    INNER JOIN document_history
+    ON t.documentId = document_history.documentId
+    AND t.updatedDate = document_history.createdDate
+    INNER JOIN document 
+    ON document_history.documentId = document.id`;
