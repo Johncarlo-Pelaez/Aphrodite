aa8d0f6de111c686fcbffa29d76dc5e6526dd339
Eduard G. Peran <egperan@epdsi.ph>
2022-01-04 18:09:38 +0800

    Add nomenclature seed sql file

---
+++ main-api/db/seeds/20220104_1806-whitelist_seed.sql
@@ -0,0 +1,11 @@
+GO
+INSERT [dbo].[nomenclature_whitelist] ([description]) VALUES (N'Developer''s Official Receipts')
+GO
+INSERT [dbo].[nomenclature_whitelist] ([description]) VALUES (N'Unit/Subdivided Tax Declaration under Buyer''s Name - Building (TD-BB)')
+GO
+INSERT [dbo].[nomenclature_whitelist] ([description]) VALUES (N'Unit/Subdivided Tax Declaration under Buyer''s Name - Lot (TD-LB)')
+GO
+INSERT [dbo].[nomenclature_whitelist] ([description]) VALUES (N'Unit/Subdivided Tax Declaration under Buyer''s Name (TD-B)')
+GO
+INSERT [dbo].[nomenclature_whitelist] ([description]) VALUES (N'Unit/Subdivided Title Under Buyer''s Name (TCT/CCT-B)')
+GO
