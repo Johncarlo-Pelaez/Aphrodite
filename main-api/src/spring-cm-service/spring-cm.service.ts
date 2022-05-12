b4591e2db4fef95569683bc17929a62c9d49a2d9
Bjorn <bvferrer@epdsi.ph>
2022-05-12 11:32:19 +0800

    adding header, agent, timeout, axios interceptor

--- main-api/src/spring-cm-service/spring-cm.service.ts
+++ main-api/src/spring-cm-service/spring-cm.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { AppConfigService } from 'src/app-config';
 import { UploadDocToSpringParams } from './spring-cm.types';
+import * as https from 'https';
+import * as http from 'http';
 
 @Injectable()
 export class SpringCMService {
   private request: AxiosInstance;
+  private options: AxiosRequestConfig;
   constructor(private readonly appConfigService: AppConfigService) {
-    this.request = axios.create({
+    this.options = {
       baseURL: this.appConfigService.salesForceURl,
       maxBodyLength: Infinity,
       maxContentLength: Infinity,
-    });
+      httpAgent: new http.Agent({ keepAlive: true }),
+      httpsAgent: new https.Agent({ keepAlive: true, sessionTimeout: 30 * 60 * 1000 }),
+      timeout: 30 * 60 * 1000,
+      maxRedirects: 10,
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+      },
+    }
+    this.request = axios.create(this.options);
   }
 
   async uploadDocToSpring(
     params: UploadDocToSpringParams,
   ): Promise<AxiosResponse> {
+    this.request.interceptors.response.use(res => {
+      if(res?.data)
+        return res;
+    }, error => {
+      return Promise.reject(error);
+    })
+
     return await this.request.post(
       `${this.appConfigService.uploadToSpringCM}`,
       { ...params },
