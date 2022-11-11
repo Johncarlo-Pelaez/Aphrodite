954dd3d4de871d9017213a55b585dc3dd845876a
Bjorn <bvferrer@epdsi.ph>
2022-11-11 14:37:55 +0800

    msal config with logs and other props

--- main-app/src/authConfig.ts
+++ main-app/src/authConfig.ts
@@ -21,9 +21,32 @@ export const buildMsalConfig = (
     system: {
       loggerOptions: {
         loggerCallback: (level, message, containsPPI) => {
-          console.log(message);
-        }, logLevel: LogLevel.Info
-      }
+          if (containsPPI) {	
+            return;	
+        }
+          switch (level) {	
+            case LogLevel.Error:	
+              console.error(message);	
+              return;	
+            case LogLevel.Info:	
+              console.info(message);	
+                return;	
+            case LogLevel.Verbose:	
+              console.debug(message);	
+              return;	
+            case LogLevel.Warning:	
+              console.warn(message);	
+              return;	
+            default:
+              return;
+          }
+        },
+      },
+      windowHashTimeout: 0,
+      iframeHashTimeout: 0,
+      loadFrameTimeout: 0,
+      tokenRenewalOffsetSeconds: 0,
+      redirectNavigationTimeout: 0,
     }
   };
 };
